5044018f63347ff88f89f70ba60f47947991f0fa
Ranol <asdhugh1@gmail.com>
2018-06-09 12:20:25 +0900

    Add GLOSSARY.md

--- src/main/kotlin/io/github/ranolp/hangeulio/vo/HangeulSyllable.kt
+++ src/main/kotlin/io/github/ranolp/hangeulio/vo/HangeulSyllable.kt
@@ -6,20 +6,7 @@ import io.github.ranolp.hangeulio.isModernHangeulSyllable
 import java.text.Normalizer
 
 /**
- * 굳어진 표현은 Hangul + Syllable[McCune-Reischauer 표기법에 의하면 han'gŭl]이나,
- * 2000년에 제정된 공식 표기법을 기준으로 하므로, Hangeul 로 칭합니다.
- *
- * 나무위키와 위키피디아에 의하면
- *
- * 보편적으로 음절 한 단위는 두음(頭音, onset)·음절핵(音節核, nucleus)·말음(末音, coda)으로 나눌 수 있다.
- * 이 중 음절핵과 말음은 합쳐서 운(韻, rhyme[2])이라고 부를 수 있다.
- * - 나무위키, 음절 문서 r226, 2.1 보편적 체계에서 발췌
- *
- * 한국어의 음절은 초성(onset), 중성(nucleus), 종성(coda)으로 이루어져 있다.
- * - 위키백과, 음절 문서 2016년 3월 11일 02:21 판, 2 한국어의 음절에서 발췌
- *
- * 다음과 같이 나타낸다고 합니다. 그러니 choseong, jungseong, jongseong 등의 음차한 표기 대신,
- * onset, nucleus, coda 등의 표기를 사용합니다.
+ * 하나의 한글 음절을 저장하는 클래스입니다.
  */
 class HangeulSyllable private constructor(
     val onset: HangeulPhoneme,
